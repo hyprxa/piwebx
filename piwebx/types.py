ddc3ac823cda7c2a05a10e8a0cbdf4c47d0661a9
newvicx <christopher.newville@hyprxa.io>
2024-01-11 15:47:00 -0500

    Update imports and type defs

--- piwebx/types.py
+++ piwebx/types.py
@@ -1,6 +1,4 @@
-from __future__ import annotations
-
-from typing import Any, Tuple, Union
+from typing import Any, Dict, List, Tuple, Union
 from typing_extensions import TypeAlias
 
 from pendulum.datetime import DateTime
@@ -11,7 +9,7 @@ __all__ = ("JSONContent", "JSONPrimitive", "TimeseriesRow")
 
 JSONPrimitive: TypeAlias = Union[str, int, float, bool, None]
 JSONContent: TypeAlias = Union[
-    JSONPrimitive, list["JSONContent"], dict[str, "JSONContent"]
+    JSONPrimitive, List["JSONContent"], Dict[str, "JSONContent"]
 ]
 
-TimeseriesRow: TypeAlias = Tuple[DateTime, list[Any]]
+TimeseriesRow: TypeAlias = Tuple[DateTime, List[Any]]
